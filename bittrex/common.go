18a7cae05dc356b03d16dd0299cc28e9e77888f1
Alessandro Sanino <saninoale@gmail.com>
2017-08-18 23:16:02 +0200

    refactoring

--- bittrex/common.go
+++ bittrex/common.go
@@ -30,39 +30,6 @@ type Auth struct {
 	PrivateKey string // The private key to connect to bittrex API.
 }
 
-// publicParams represents the possible public parameters that can
-// be passed for public Api calls.
-type publicParams struct {
-	MarketName   *string
-	TickInterval *string
-}
-
-// AddToQueryString adds the non empty fields of the publicParams struct
-// to the specified query string.
-func (pp publicParams) AddToQueryString(queryString url.Values) {
-	if queryString != nil {
-		if pp.MarketName != nil {
-			queryString.Add("marketName", *pp.MarketName)
-		}
-		if pp.TickInterval != nil {
-			queryString.Add("tickInterval", *pp.TickInterval)
-		}
-	}
-}
-
-// privateParams represents the possible private parameters that can
-// be passed for auth API calls.
-type privateParams struct {
-}
-
-// AddToPostForm adds the non empty fields of the publicParams struct
-// to the specified post form.
-func (pp privateParams) AddToPostForm(postForm url.Values) {
-	if postForm != nil {
-
-	}
-}
-
 // defaultConnOpts represents the default configuration for ConnectOptions.
 var defaultConnOpts ConnectOptions
 
