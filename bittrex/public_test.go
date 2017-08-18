510619333672fdfce018b61fb2c2bfdbbc5d90d0
Alessandro Sanino <saninoale@gmail.com>
2017-08-18 23:23:37 +0200

    refactoring

---
+++ bittrex/public_test.go
@@ -0,0 +1,30 @@
+package bittrex_test
+
+import (
+	"testing"
+
+	"github.com/saniales/golang-bittrex-api/bittrex"
+)
+
+var latestTestedVersion = "2.11"
+
+func TestGetServerAPIVersion(t *testing.T) {
+	testIsAPIAlive(t)
+	version, err := bittrex.GetServerAPIVersion()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if version != latestTestedVersion {
+		t.Errorf(`Please check version you are testing, on server it is %s, 
+while on this client it is %s.`,
+			latestTestedVersion, version)
+	}
+}
+
+func TestGetBTCPrice(t *testing.T) {
+	testIsAPIAlive(t)
+	_, err := bittrex.GetBTCPrice()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
